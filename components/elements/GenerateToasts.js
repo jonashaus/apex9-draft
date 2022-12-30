219b13b2101ad27b62f7593de80447931da10116
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-12-30 11:35:43 +0500

    - added auto close delay

--- components/elements/GenerateToasts.js
+++ components/elements/GenerateToasts.js
@@ -1,26 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AppContext } from "../../context/AppContextProvider";
 
-const Toast = ({ colorScheme, text, onDismiss }) => (
-  <div
-    className={`toast show align-items-center text-bg-${colorScheme} border-0`}
-    data-bs-autohide="true"
-    data-bs-delay="1000"
-    role="alert"
-    aria-live="assertive"
-    aria-atomic="true"
-  >
-    <div className="d-flex">
-      <div className="toast-body">{text}</div>
-      <button
-        type="button"
-        className="btn-close btn-close-white me-2 m-auto"
-        aria-label="Close"
-        onClick={onDismiss}
-      />
+const Toast = ({ colorScheme, text, onDismiss, toastIndex }) => {
+  const { handleRemoveToast } = useContext(AppContext);
+
+  const handleAutoClose = () => {
+    const timer = setTimeout(() => {
+      handleRemoveToast(toastIndex);
+    }, 3000);
+
+    return timer;
+  };
+
+  useEffect(() => {
+    const timer = handleAutoClose();
+
+    () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`toast show align-items-center text-bg-${colorScheme} border-0`}
+      data-bs-autohide="true"
+      data-bs-delay="1000"
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
+      <div className="d-flex">
+        <div className="toast-body">{text}</div>
+        <button
+          type="button"
+          className="btn-close btn-close-white me-2 m-auto"
+          aria-label="Close"
+          onClick={onDismiss}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const GenerateToasts = () => {
   const { toasts, handleRemoveToast } = useContext(AppContext);
@@ -30,6 +50,7 @@ const GenerateToasts = () => {
       {toasts.map((toast, i) => (
         <Toast
           key={`toast_${i}`}
+          toastIndex={i}
           colorScheme={toast.color}
           text={toast.text}
           onDismiss={() => handleRemoveToast(i)}
